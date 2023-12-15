efca7f0f2174ebbb523505b9e470df8253eaaba3
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-15 16:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	 let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	return str && pattern.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+// 
